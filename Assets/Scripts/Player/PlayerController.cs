d58c7ddec41f1d3858943c7adb7347fc41736f2d
jmjedi <s251620@students.aie.edu.au>
2026-06-25 20:21:11 +0930

    tiny update for player

--- Assets/Scripts/Player/PlayerController.cs
+++ Assets/Scripts/Player/PlayerController.cs
@@ -7,10 +7,12 @@ using UnityEngine.InputSystem;
 [RequireComponent(typeof(Rigidbody))]
 public class PlayerController : MonoBehaviour
 {
+    //Get player Values
     private PlayerInput controls;
     private Vector2 moveInput;
     private Vector3 PlayerMovementInput;
 
+    //Get Components
     [SerializeField] private Rigidbody PlayerBody;
     [SerializeField] private Transform FeetTransform;
 
@@ -23,6 +25,7 @@ public class PlayerController : MonoBehaviour
     [SerializeField] private float rayDist = 3.0f;
     [SerializeField] private float maxSlopeAngle = 50f;
 
+    //Flags
     private bool canDodge = true;
 
     //SLOPE BASED
@@ -35,11 +38,13 @@ public class PlayerController : MonoBehaviour
 
     private void Awake()
     {
+        //Get Controls
         controls = new PlayerInput();
     }
 
     private void OnEnable()
     {
+        //Enable Controls
         controls.Gameplay.Enable();
         if (canDodge)
             controls.Gameplay.Dodge.started += DodgeActive;
@@ -49,33 +54,39 @@ public class PlayerController : MonoBehaviour
 
     private void OnDisable()
     {
+        //Disable Controls
         controls.Gameplay.Disable();
-
     }
 
     private void Update()
     {
+        //Get player input stick
         moveInput = controls.Gameplay.Move.ReadValue<Vector2>();
         PlayerMovementInput = new Vector3(moveInput.x, 0, moveInput.y);
 
         Vector3 moveVector = transform.TransformDirection(PlayerMovementInput) * Speed;
-
+        //Check if we are on a slope to slow down player
         if (isOnSlope)
         {
-            float ogSpd = moveVector.magnitude / 2;
-            moveVector = Vector3.ProjectOnPlane(moveVector, slopeHit.normal).normalized * ogSpd;
+            if (PlayerBody.velocity.y < 0) //Going up
+                moveVector = Vector3.ProjectOnPlane(moveVector, slopeHit.normal).normalized * Speed;
+            else //Going down
+                moveVector = Vector3.ProjectOnPlane(moveVector, slopeHit.normal).normalized * Speed;
+            PlayerBody.velocity = moveVector;
+        }
+        else
+        {
+            //Normal Velocity
+            PlayerBody.velocity = new Vector3(moveVector.x, PlayerBody.velocity.y, moveVector.z);
         }
 
-        PlayerBody.velocity = new Vector3(moveVector.x, PlayerBody.velocity.y, moveVector.z);
         CheckGroundAndSlope();
     }
 
     private void FixedUpdate()
     {
-        
         Vector3 gravityForce = Physics.gravity * FallForce;
         PlayerBody.AddForce(gravityForce, ForceMode.Acceleration);
-        ///AlignWithFloor();
     }
 
     private void CheckGroundAndSlope()
